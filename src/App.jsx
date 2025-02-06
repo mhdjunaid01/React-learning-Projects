e3e05197e9aabb41e4d92404af2184f3b0218706
mhdjunaid01 <muhammedjunaid2016.com>
2025-02-06 17:25:35 +0530

    Add new Project folder CalaculatorProject4 files to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 // import Todo from './Component/ToDoProject2/Todo'
-import Meals from './Component/MealsAPI.Peoject3/Meals'
+// import Meals from './Component/MealsAPI.Peoject3/Meals'
+import Calculator from './Component/CalculatorPoject4/Calculator'
 // import Counter from './Component/CounterProject1/Counter'
 
 
@@ -9,8 +10,8 @@ const App = () => {
     <div>
       {/* <Counter/> */}
       {/* <Todo/> */}
-      <Meals/>
-
+      {/* <Meals/> */}
+      <Calculator/>
     </div>
   )
 }
