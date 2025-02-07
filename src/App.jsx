a42d4572850caaf213490468132613a182ace70b
mhdjunaid01 <muhammedjunaid2016.com>
2025-02-07 14:08:17 +0530

    add new project folder AccordionData Project  7

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-// import ColorToggler from './Component/Color Toggler project 5/ColorToggler'
-// import Todo from './Component/ToDoProject2/Todo'
-// import Meals from './Component/MealsAPI.Peoject3/Meals'
-// import Calculator from './Component/CalculatorPoject4/Calculator'
-import HiddenSearchBar from './Component/HiddenSearchBar project  6/HiddenSearchBar'
-// import Counter from './Component/CounterProject1/Counter'
-
+import React from 'react';
+// import ColorToggler from './Component/Color Toggler project 5/ColorToggler';
+// import Todo from './Component/ToDoProject2/Todo';
+// import Meals from './Component/MealsAPI.Peoject3/Meals';
+// import Calculator from './Component/CalculatorPoject4/Calculator';
+// import HiddenSearchBar from './Component/HiddenSearchBar project  6/HiddenSearchBar';
+// import Counter from './Component/CounterProject1/Counter';
+import AccordionData from './Component/AccordionData Project  7/AccordionData';
+import { accordionData } from './utils/content';
 
 const App = () => {
   return (
@@ -15,9 +16,16 @@ const App = () => {
       {/* <Meals/> */}
       {/* <Calculator/> */}
       {/* <ColorToggler/> */}
-      <HiddenSearchBar/>
+      {/* <HiddenSearchBar/> */}
+      <div className="accordionData">
+        {
+          accordionData.map(({ title, content }) => (
+            <AccordionData key={title} title={title} content={content} />
+          ))
+        }
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Component/AccordionData Project  7/AccordionData.jsx
+++ src/Component/AccordionData Project  7/AccordionData.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./style.css";
 
-const AccordionData = () => {
+const AccordionData = ({ title, content }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleToggle = () => {
+    setIsActive(!isActive);
+    
+  };
+
   return (
-    <div>AccordionData</div>
-  )
-}
+    <section className="accordion-card">
+      <div className="header" onClick={handleToggle}>
+        <div>{title}</div>
+        <p className="icon">{isActive ? "-" : "+"}</p>
+      </div>
+      <div className="content">
+        {isActive && <p className="card-info">{content}</p>}
+      </div>
+    </section>
+  );
+};
 
-export default AccordionData
+export default AccordionData;
