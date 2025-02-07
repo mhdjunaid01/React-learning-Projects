8f4f2f584bc6338b73502821263076ef883d236b
mhdjunaid01 <muhammedjunaid2016.com>
2025-02-07 12:38:30 +0530

    add new folder hiddenSearch project 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import ColorToggler from './Component/Color Toggler project 5/ColorToggler'
+// import ColorToggler from './Component/Color Toggler project 5/ColorToggler'
 // import Todo from './Component/ToDoProject2/Todo'
 // import Meals from './Component/MealsAPI.Peoject3/Meals'
-import Calculator from './Component/CalculatorPoject4/Calculator'
+// import Calculator from './Component/CalculatorPoject4/Calculator'
+import HiddenSearchBar from './Component/HiddenSearchBar project  6/HiddenSearchBar'
 // import Counter from './Component/CounterProject1/Counter'
 
 
@@ -13,7 +14,8 @@ const App = () => {
       {/* <Todo/> */}
       {/* <Meals/> */}
       {/* <Calculator/> */}
-      <ColorToggler/>
+      {/* <ColorToggler/> */}
+      <HiddenSearchBar/>
     </div>
   )
 }
---
+++ src/Component/AccordionData Project  7/AccordionData.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AccordionData = () => {
+  return (
+    <div>AccordionData</div>
+  )
+}
+
+export default AccordionData
